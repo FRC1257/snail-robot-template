46dec4ac9fa73ae83fb9ec96e82a7ffb905c5242
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2024-01-12 14:49:18 -0500

    update talon code

--- src/main/java/frc/robot/subsystems/drive/DriveIOTalon.java
+++ src/main/java/frc/robot/subsystems/drive/DriveIOTalon.java
@@ -13,94 +13,38 @@
 
 package frc.robot.subsystems.drive;
 
-import com.ctre.phoenix6.BaseStatusSignal;
-import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.VoltageOut;
-import com.ctre.phoenix6.hardware.Pigeon2;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.*;
+
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 
 public class DriveIOTalon implements DriveIO {
-    private static final double GEAR_RATIO = 6.0;
-
-    private final TalonFX leftLeader = new TalonFX(4);
-    private final TalonFX leftFollower = new TalonFX(5);
-    private final TalonFX rightLeader = new TalonFX(6);
-    private final TalonFX rightFollower = new TalonFX(8);
-
-    private final StatusSignal<Double> leftPosition = leftLeader.getPosition();
-    private final StatusSignal<Double> leftVelocity = leftLeader.getVelocity();
-    private final StatusSignal<Double> leftAppliedVolts = leftLeader.getMotorVoltage();
-    private final StatusSignal<Double> leftLeaderCurrent = leftLeader.getStatorCurrent();
-    private final StatusSignal<Double> leftFollowerCurrent = leftFollower.getStatorCurrent();
-
-    private final StatusSignal<Double> rightPosition = rightLeader.getPosition();
-    private final StatusSignal<Double> rightVelocity = rightLeader.getVelocity();
-    private final StatusSignal<Double> rightAppliedVolts = rightLeader.getMotorVoltage();
-    private final StatusSignal<Double> rightLeaderCurrent = rightLeader.getStatorCurrent();
-    private final StatusSignal<Double> rightFollowerCurrent = rightFollower.getStatorCurrent();
-
+    private final WPI_VictorSPX leftLeader = new WPI_VictorSPX(4);
+    private final WPI_VictorSPX leftFollower = new WPI_VictorSPX(5);
+    private final WPI_VictorSPX rightLeader = new WPI_VictorSPX(6);
+    private final WPI_VictorSPX rightFollower = new WPI_VictorSPX(8);
 
     private GyroIOReal gyro;
 
-
     public DriveIOTalon() {
-        var config = new TalonFXConfiguration();
-        config.CurrentLimits.StatorCurrentLimit = 30.0;
-        config.CurrentLimits.StatorCurrentLimitEnable = true;
-        config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-        leftLeader.getConfigurator().apply(config);
-        leftFollower.getConfigurator().apply(config);
-        rightLeader.getConfigurator().apply(config);
-        rightFollower.getConfigurator().apply(config);
-        leftFollower.setControl(new Follower(leftLeader.getDeviceID(), false));
-        rightFollower.setControl(new Follower(rightLeader.getDeviceID(), false));
+        leftFollower.follow(leftLeader);
+        rightFollower.follow(rightLeader);
 
-        BaseStatusSignal.setUpdateFrequencyForAll(
-                100.0, leftPosition, rightPosition); // Required for odometry, use faster rate
-        BaseStatusSignal.setUpdateFrequencyForAll(
-                50.0,
-                leftVelocity,
-                leftAppliedVolts,
-                leftLeaderCurrent,
-                leftFollowerCurrent,
-                rightVelocity,
-                rightAppliedVolts,
-                rightLeaderCurrent,
-                rightFollowerCurrent);
-        leftLeader.optimizeBusUtilization();
-        leftFollower.optimizeBusUtilization();
-        rightLeader.optimizeBusUtilization();
-        rightFollower.optimizeBusUtilization();
+        leftFollower.setNeutralMode(NeutralMode.Brake);
+        leftFollower.setNeutralMode(NeutralMode.Coast);
+        rightLeader.setNeutralMode(NeutralMode.Brake);
+        rightFollower.setNeutralMode(NeutralMode.Coast);
 
         gyro = GyroIOReal.getInstance();
     }
 
     @Override
     public void updateInputs(DriveIOInputs inputs) {
-        BaseStatusSignal.refreshAll(
-                leftPosition,
-                leftVelocity,
-                leftAppliedVolts,
-                leftLeaderCurrent,
-                leftFollowerCurrent,
-                rightPosition,
-                rightVelocity,
-                rightAppliedVolts,
-                rightLeaderCurrent,
-                rightFollowerCurrent
-                );
-
-        inputs.leftPositionRad = Units.rotationsToRadians(leftPosition.getValueAsDouble()) / GEAR_RATIO;
-        inputs.leftVelocityRadPerSec = Units.rotationsToRadians(leftVelocity.getValueAsDouble()) / GEAR_RATIO;
-        inputs.rightPositionRad = Units.rotationsToRadians(rightPosition.getValueAsDouble()) / GEAR_RATIO;
-        inputs.rightVelocityRadPerSec = Units.rotationsToRadians(rightVelocity.getValueAsDouble()) / GEAR_RATIO;
-
         inputs.gyroYawRad = gyro.getYawAngle();
+        inputs.leftPositionRad = 0;
+        inputs.rightPositionRad = 0;
+        inputs.leftVelocityRadPerSec = leftLeader.getBusVoltage();
+        inputs.rightVelocityRadPerSec = rightLeader.getBusVoltage();
         inputs.gyroRollPitchYawRad[0] = gyro.getRollAngle();
         inputs.gyroRollPitchYawRad[1] = gyro.getPitchAngle();
         inputs.gyroRollPitchYawRad[2] = gyro.getYawAngle();
@@ -108,7 +52,27 @@ public class DriveIOTalon implements DriveIO {
 
     @Override
     public void setVoltage(double leftVolts, double rightVolts) {
-        leftLeader.setControl(new VoltageOut(leftVolts));
-        rightLeader.setControl(new VoltageOut(rightVolts));
+        leftLeader.set(leftVolts);
+        rightLeader.set(rightVolts);
+    }
+
+    /* No encoders so no position or velocity control works */
+    @Override
+    public double getLeftPositionMeters() {
+        return 0;
+    }
+
+    @Override
+    public double getRightPositionMeters() {
+        return 0;
+    }
+
+    @Override
+    public void setVelocity(DifferentialDriveWheelSpeeds wheelSpeeds) {
+    }
+
+    @Override
+    public void zero() {
+        GyroIOReal.getInstance().zeroAll();
     }
 }
